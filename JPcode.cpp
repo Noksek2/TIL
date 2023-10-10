eca16767259cb5eb6db068cf43e53fb49f4961c4
Noksek2 <banwonkim@naver.com>
2023-10-10 14:02:23 +0900

    Update JPcode.cpp

--- JPcode.cpp
+++ JPcode.cpp
@@ -77,7 +77,7 @@ ma mi mu me mo
 mya myu mye myo
 ra ri ru re ro
 rya ryu rye ryo
-wa wi we wo 
+wa wi we wo
 
 Cy y V Ce?
 C1 V Ce?
@@ -85,26 +85,49 @@ V Ce?
 
 
 */
-enum{
+enum {
 	rule_Cy,
-	rule_V=1,
-	rule_C1=2,
-	rule_Cend=3,
+	rule_V = 1,
+	rule_C1 = 2,
+	rule_Cend = 3,
 };
 CHAR man[4] = { 0,0,0,0 };
-CHAR rule[][50] = { "kgnhbpmr","aiueo","kgszjtdcnhbpmryw","nt"};
+CHAR rule[][50] = { "kgnhbpmr","aiueo","kgszjtdcnhbpmryw","nt" };
 char sylls[4096][4];
 class GetCode {
 public:
 	wchar_t unimap[128][5] = { {0} };
 	wchar_t head, mid, tail;
-	void initvowel(char c,wchar_t code,UCHAR type=2) {
-		for (int i = 0; i < 5; i++) {
-			unimap[c][i] = code + i * type;
+	void initvowel(char c, wchar_t code, UCHAR type = 2) {
+		if (c == 't' || c == 'd') {
+			unimap[c][0] = code;
+			unimap[c][1] = code+2;
+			unimap[c][2] = code + 5;
+			unimap[c][3] = code + 7;
+			unimap[c][4] = code + 9;
+		}
+		else if (c == 'w') {
+			unimap[c][0] = code;
+			unimap[c][1] = code + 1;
+			unimap[c][2] = 0;
+			unimap[c][3] = code + 2;
+			unimap[c][4] = code + 3;
+		}
+		else if (c == 'y') {
+			unimap[c][0] = code;
+			unimap[c][1] = 0;
+			unimap[c][2] = code + 2;
+			unimap[c][3] = 0;
+			unimap[c][4] = code + 4;
+		}
+		else {
+			for (int i = 0; i < 5; i++) {
+				unimap[c][i] = code + i * type;
+			}
 		}
 	}
 	GetCode() {
-		
+
 		//unimap[0][0] = 0x3042;
 		initvowel(0, 0x3042);
 		unimap['a'][0] = 0;
@@ -112,52 +135,66 @@ public:
 		unimap['u'][0] = 2;
 		unimap['e'][0] = 3;
 		unimap['o'][0] = 4;
-		initvowel('k',0x304B);
-		initvowel('g',0x304C);
-		initvowel('s',0x3055);
-		initvowel('z',0x3056);
-		initvowel('t',0x305f);
-		initvowel('d',0x3060);
-		initvowel('n',0x306A);
-		initvowel('h',0x306F,3);
-		initvowel('b',0x3070,3);
-		initvowel('p',0x3071,3);
-		initvowel('m',0x307E);
+		initvowel('k', 0x304B);
+		initvowel('g', 0x304C);
+		initvowel('s', 0x3055);
+		initvowel('z', 0x3056);
+		initvowel('t', 0x305f);
+		initvowel('d', 0x3060);
+		initvowel('n', 0x306A,1);
+		initvowel('h', 0x306F, 3);
+		initvowel('b', 0x3070, 3);
+		initvowel('p', 0x3071, 3);
+		initvowel('m', 0x307E,1);
 
-		initvowel('r', 0x3089);
+		initvowel('r', 0x3089,1);
 
 		initvowel('w', 0x308F);
 		initvowel('y', 0x3084);
-		initvowel('c', 0x3061);
-		initvowel('x', 0x3064);
+
+		//initvowel('c', 0x3061);
+		//initvowel('x', 0x3064);
 	}
 	wchar_t get_Cend(char c) {
 		if (c == 'n')return 0x3093;
 		else if (c == 't')return 0x3063;
 		return 0;
 	}
-	wchar_t getcode_Head(char* syll) {
+	wchar_t get_Yvowel(char c) {
+		switch (c) {
+		case 'a':return 0x3083;
+		case 'u':return 0x3085;
+		case 'e':return 0x3047;
+		case 'o':return 0x3087;
+		}
+		return 0;
+	}
+	void getcode_HeadMid(char* syll) {
+		head = mid = tail = 0;
 		if (syll[1] == 'y') {
-			return unimap[syll[0]][1];
+			head = unimap[syll[0]][1];
+			mid = get_Yvowel(syll[2]);
 		}
-		else if (syll[0] == 'x')return 0x3064;
-		return unimap[syll[0]][syll[1]];
-	}
-	wchar_t getcode_Mid(char* syll) {
-		if (syll[0] == 'c' || syll[0] == '$' || syll[0] == 'j') {
-			return unimap[syll[0]][1];
+		else if (syll[0] == 'x') {
+			head = 0x3064;
 		}
-		else if (syll[1] == 'y') {
-			unimap[syll[0]][1];
+		else if (syll[0] == 'c') {
+			head = unimap['t'][1];
+			mid = get_Yvowel(syll[2]);
 		}
-	}
-	wchar_t getcode_Tail (char* syll) {
-		return get_Cend(syll[3]);
+		else if (syll[0] == 'j') {
+			head = unimap['z'][1];
+			mid = get_Yvowel(syll[2]);
+		}
+		else if (syll[0] == '$') {
+			head = unimap['s'][1];
+			mid = get_Yvowel(syll[2]);
+		}
+		else head = unimap[syll[0]][unimap[syll[2]][0]];
 	}
 	void getcode(char* syll) {
-		head = getcode_Head(syll);
-		mid = getcode_Mid(syll);
-		tail = getcode_Tail(syll);
+		getcode_HeadMid(syll);
+		tail = get_Cend(syll[3]);
 	}
 };
 bool match_rule(CHAR c, UCHAR type) {
@@ -166,9 +203,9 @@ bool match_rule(CHAR c, UCHAR type) {
 	}
 	return false;
 }
-void get_syllable(UINT &syllsize,char* str) {//음절
+void get_syllable(UINT& syllsize, char* str) {//음절
 	for (CHAR* c = str; *c != '\0';) {
-		while (*c == ' ' || *c == '\'')c++;
+		while (*c == ' ' ||* c == '\n' ||* c == '\t' || *c == '\'')c++;
 		[&]() {
 			if (*(c + 1) == 'h') {
 				if (*c == 's')man[0] = '$';
@@ -180,58 +217,58 @@ void get_syllable(UINT &syllsize,char* str) {//음절
 				man[0] = 'x';
 				c += 2;
 			}
-		}();//sh ch
-		if (man[0] != 0)goto VOWEL;
-		if (*(c + 1) == 'y') {
-			if (match_rule(*c, rule_Cy)) {
-				man[0] = *c; man[1] = 'y';
-				c += 2;
+			}();//sh ch
+			if (man[0] != 0)goto VOWEL;
+			if (*(c + 1) == 'y') {
+				if (match_rule(*c, rule_Cy)) {
+					man[0] = *c; man[1] = 'y';
+					c += 2;
+				}
 			}
-		}
-		else if (match_rule(*c, rule_C1)) {
-			man[0] = *c;
-			c++;
-		}
-	VOWEL:
-		if (match_rule(*c, rule_V)) {
-			man[2] = *c;
-			c++;
-		}
-		else {
-			cout << "\n==모음 획득 실패==";
-			break;
-		}
-
-		if (match_rule(*c, rule_Cend)) {
-			if (*c == 't' && *(c + 1) == 's');
-			else if (!match_rule(*(c + 1), rule_V)) {
-				man[3] = *c;
+			else if (match_rule(*c, rule_C1)) {
+				man[0] = *c;
 				c++;
 			}
-		}
-		else if (*c == *(c + 1) && (*c == 'p' || *c == 's' || *c == 'k')) {
-			man[3] = 't';
-			c++;
-		}
-		if (man[0] == '$' && man[1] == 'i')man[0] = 's';
-		
-		sylls[syllsize][0] = man[0];
-		sylls[syllsize][1] = man[1];
-		sylls[syllsize][2] = man[2];
-		sylls[syllsize++][3] = man[3];
-		man[0] = man[1] = man[2] = man[3] = 0;
+		VOWEL:
+			if (match_rule(*c, rule_V)) {
+				man[2] = *c;
+				c++;
+			}
+			else {
+				cout << "\n==모음 획득 실패==";
+				break;
+			}
+
+			if (match_rule(*c, rule_Cend)) {
+				if (*c == 't' && *(c + 1) == 's');
+				else if (!match_rule(*(c + 1), rule_V)) {
+					man[3] = *c;
+					c++;
+				}
+			}
+			else if (*c == *(c + 1) && (*c == 'p' || *c == 's' || *c == 'k')) {
+				man[3] = 't';
+				c++;
+			}
+			if (man[0] == '$' && man[1] == 'i')man[0] = 's';
+
+			sylls[syllsize][0] = man[0];
+			sylls[syllsize][1] = man[1];
+			sylls[syllsize][2] = man[2];
+			sylls[syllsize++][3] = man[3];
+			man[0] = man[1] = man[2] = man[3] = 0;
 	}
 }
 void main() {
 	CHOOSEFONT font = { 0 };
-	LOGFONT logfont; 
+	LOGFONT logfont;
 	font.lStructSize = sizeof(CHOOSEFONT);
-	font.hwndOwner = GetConsoleWindow() ;
+	font.hwndOwner = GetConsoleWindow();
 	font.iPointSize = 100;
 	font.Flags = CF_EFFECTS | CF_SCREENFONTS;
 	font.lpLogFont = &logfont;
 	//if (ChooseFont(&font)!=0) {}
-	
+
 	setlocale(LC_ALL, 0);
 	CHAR* str;
 	FILE* file;
@@ -242,29 +279,39 @@ void main() {
 		return;
 	}
 	fsize = readfile(file, str);
-	fclose(file);	
+	fclose(file);
 	puts(str);
 
 	/*음절 전부 획득하기*/
-	UINT syllsize=0;
-	get_syllable(syllsize,str);
+	UINT syllsize = 0;
+	get_syllable(syllsize, str);
 
 	for (UINT i = 0; i < syllsize; i++) {
-		for(int j=0;j<4;j++)
+		for (int j = 0;j < 4;j++)
 			if (sylls[i][j] != 0)putchar(sylls[i][j]);
 		putchar(' ');
 	}
 	{
-		GetCode *getcode;
+		GetCode* getcode;
 		getcode = new GetCode();
 
 		std::wofstream wof;
 		wof.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::generate_header>));
-
 		wof.open(L"a.out");
+		for (UCHAR c = 0;c < 128;c++) {
+			for (int j = 0;j < 5;j++)
+				wof << getcode->unimap[c][j];
+			wof << '\n';
+		}
 		for (UINT i = 0; i < syllsize; i++) {
+			for (int j = 0;j < 4;j++)
+				if (sylls[i][j] != 0)wof << sylls[i][j];
+			
 			getcode->getcode(sylls[i]);
-			wof <<getcode->head<<'\0';
+			if(getcode->head)wof << getcode->head;
+			if (getcode->mid)wof << getcode->mid;
+			if (getcode->tail)wof << getcode->tail;
+			wof << ' ';
 		}
 		wof << L"終";
 
